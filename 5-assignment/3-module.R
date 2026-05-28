b025c4ab3396b9c3c40e44c6aa6b87a57a5b5265
emma-ras <emmarasmussen@ucsb.edu>
2026-05-28 12:21:00 -0700

    add parameter settings for all calls

--- 5-assignment/3-module.R
+++ 5-assignment/3-module.R
@@ -1,7 +1,7 @@
 
 # size, system size (kW) 
 # om, estimated operations and maintenance ($/kW/year)
-# # time in the future that cost/value occurs (years)
+# time in the future that cost/value occurs (years)
 dis_om = function(om, size, year, rate) {
   # use value to compute NPV
   result <- numeric(year)
