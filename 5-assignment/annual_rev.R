4a23dc6c4d0296532c43e73ca17715e85c218761
emma-ras <emmarasmussen@ucsb.edu>
2026-05-28 08:23:16 -0700

    updated hw 5

---
+++ 5-assignment/annual_rev.R
@@ -0,0 +1,10 @@
+################################################################################
+### Emma Rasmussen 
+### Module 1: Annual Revenue
+
+annual_rev <- (lmp, kw) {
+  total_hours <- 0 
+  
+  for (hour in lmp)
+  result <- loop
+}
