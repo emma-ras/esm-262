020eefabf990b23510cfe6d046a4cdc98b838d6a
emma-ras <emmarasmussen@ucsb.edu>
2026-06-09 14:13:08 -0700

    updating documentation Roxyen

--- 5-assignment/1_module.R
+++ 5-assignment/1_module.R
@@ -1,16 +1,19 @@
-# annual_rev = Sum(hourly kW * ($/KW) [use For loop to sum (8760 values)]
-# lmp = locational marginal price for nearest nodes ($/kw)
-# kwh = estimated hourly production of energy (kwh)
+#' Annual Revenue
+#'
+#' compute annual revenue from annual hourly price and annual hourly kwh production
+#' @param lmp  locational marginal price for nearest nodes ($/kw)
+#' @param kwh description estimated hourly production of energy (kwh)
+#' @references value in $
+#' @author Emma Rasmussen
+#' @export
 
 annual_rev <- function(lmp, kwh) {
- stopifnot(length(lmp) == 8760, length(kwh) == 8760) # check for full data annual data 
+  stopifnot(length(lmp) == 8760, length(kwh) == 8760) # check for full data annual data
   
-  hourly_rev <- numeric(8760) # create storage vector 
+  hourly_rev <- numeric(8760) # create storage vector
   for (i in 1:8760) {
-    hourly_rev[i]<- lmp[i] * kwh[i] # expected per hour revenue 
+    hourly_rev[i]<- lmp[i] * kwh[i] # expected per hour revenue
   }
-return(sum(hourly_rev))
+  return(sum(hourly_rev))
 }
-
-
 
